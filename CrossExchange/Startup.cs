7b6e3db95f11c17ffdd1a99534804bc74f0c93b4
Muhammad Anees <muhammadanis2007@gmail.com>
2018-09-02 05:48:04 +0500

    second fix

--- CrossExchange/Startup.cs
+++ CrossExchange/Startup.cs
@@ -11,7 +11,7 @@ namespace CrossExchange
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
